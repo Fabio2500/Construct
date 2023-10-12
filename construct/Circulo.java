9b824cbfc0078f63f9efc6ca6b22c6593a22016a
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2023-10-12 10:08:07 -0300

    Add files via upload

---
+++ construct/Circulo.java
@@ -0,0 +1,15 @@
+class Circulo{
+	 float raio;
+	 float diametro;
+
+    public void mostrar_dados(){
+    	System.out.println("raio :" + raio);
+    	System.out.println("diametro: " + diametro);
+    }
+	public Circulo(float raio){
+    this.raio = raio;
+    this.diametro = raio*2;
+    this.mostrar_dados();
+	}
+
+}
---
+++ construct/LerCirculo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class LerCirculo{
+    public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("digite o raio:");
+	Circulo circulo1 = new Circulo(sc.nextFloat());
+	
+
+
+
+
+	}
+}
